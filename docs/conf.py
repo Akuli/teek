94e80f58377da1a854a29273381b7f224f80d37f
Akuli <akuviljanen17@gmail.com>
2018-08-18 14:16:22 +0300

    more find an d replace

--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,12 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))    # for extensions.py
-sys.path.insert(0, os.path.abspath('..'))   # for tkinder
+sys.path.insert(0, os.path.abspath('..'))   # for pythotk
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Tkinder'
+project = 'Pythotk'
 copyright = '2018, Akuli'
 author = 'Akuli'
 
@@ -109,7 +109,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Tkinderdoc'
+htmlhelp_basename = 'Pythotkdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -136,7 +136,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Tkinder.tex', 'Tkinder Documentation',
+    (master_doc, 'Pythotk.tex', 'Pythotk Documentation',
      'Akuli', 'manual'),
 ]
 
@@ -146,7 +146,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'tkinder', 'Tkinder Documentation',
+    (master_doc, 'pythotk', 'Pythotk Documentation',
      [author], 1)
 ]
 
@@ -157,8 +157,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Tkinder', 'Tkinder Documentation',
-     author, 'Tkinder', 'One line description of project.',
+    (master_doc, 'Pythotk', 'Pythotk Documentation',
+     author, 'Pythotk', 'One line description of project.',
      'Miscellaneous'),
 ]
 
