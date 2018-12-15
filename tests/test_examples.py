eba97ae330a11c076599be42f0da76fba2a65a8c
Akuli <akuviljanen17@gmail.com>
2018-12-15 17:18:27 +0200

    example tester

---
+++ tests/test_examples.py
@@ -0,0 +1,31 @@
+import os
+import pythotk
+
+
+EXAMPLES_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'examples')
+
+
+# magic ftw
+def _create_test_function(filename):
+    with open(os.path.join(EXAMPLES_DIR, filename), 'r') as file:
+        code = file.read()
+
+    def func(monkeypatch):
+        with monkeypatch.context() as monkey:
+            monkey.setattr(pythotk, 'run', (lambda: None))
+            exec(code, {})
+
+        # make sure that nothing breaks if the real .run() is called
+        pythotk.update()
+        pythotk.after_idle(pythotk.quit)
+        pythotk.run()
+
+    func.__name__ = func.__qualname__ = 'test_' + filename.replace('.', '_')
+    globals()[func.__name__] = func
+
+
+for filename in os.listdir(EXAMPLES_DIR):
+    if filename.endswith('.py') and not filename.startswith('_'):
+        _create_test_function(filename)
