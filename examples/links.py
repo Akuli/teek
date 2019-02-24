400888ca9d93c655796758fc2bcc5bd7bcc086ec
Akuli <akuviljanen17@gmail.com>
2019-02-24 17:36:47 +0200

    new way to use links that lets you link to anything, not just text

--- examples/links.py
+++ examples/links.py
@@ -9,11 +9,20 @@ text.pack()
 def lol():
     teek.dialog.info("Link Example Dialog", "Lol")
 
-links.add_url_link(text, "Docs", 'https://teek.rtfd.io/')
+old_end = text.end
+text.insert(text.end, "Docs")
+links.add_url_link(text, 'https://teek.rtfd.io/', old_end, text.end)
 text.insert(text.end, '\n\n')
-links.add_url_link(text, "GitHub", 'https://github.com/Akuli/teek')
+
+old_end = text.end
+text.insert(text.end, "GitHub")
+links.add_url_link(text, 'https://github.com/Akuli/teek', old_end, text.end)
 text.insert(text.end, '\n\n')
-links.add_function_link(text, "Lol", lol)
+
+old_end = text.end
+text.insert(text.end, "Lol")
+links.add_function_link(text, lol, old_end, text.end)
+text.insert(text.end, '\n\n')
 
 window.on_delete_window.connect(teek.quit)
 teek.run()
--- tests/extras/test_links.py
+++ tests/extras/test_links.py
@@ -7,18 +7,21 @@ from teek.extras import links
 
 def test_links_clicking():
     text = teek.Text(teek.Window())
+    text.insert(text.end, 'Blah')
 
     stuff = []
     links.add_function_link(
-        text, 'abc', functools.partial(stuff.append, 1))
+        text, functools.partial(stuff.append, 1), (1, 0), (1, 2))
     links.add_function_link(
-        text, '123', functools.partial(stuff.append, 2), (1, 2))
-    assert text.get() == 'ab123c'
+        text, functools.partial(stuff.append, 2), (1, 2), (1, 4))
 
     all_tag_names = (tag.name for tag in text.get_all_tags())
     assert {'teek-extras-link-1',
             'teek-extras-link-2',
             'teek-extras-link-common'}.issubset(all_tag_names)
+
+    assert text.get_tag('teek-extras-link-1').ranges() == [((1, 0), (1, 2))]
+    assert text.get_tag('teek-extras-link-2').ranges() == [((1, 2), (1, 4))]
 
     text.get_tag('teek-extras-link-1').bindings['<1>'].run(None)
     text.get_tag('teek-extras-link-2').bindings['<1>'].run(None)
@@ -28,8 +31,9 @@ def test_links_clicking():
 def test_links_cursor_changes():
     text = teek.Text(teek.Window())
     text.config['cursor'] = 'clock'
+    text.insert(text.end, 'abc')
 
-    links.add_function_link(text, 'abc', print)
+    links.add_function_link(text, print, text.start, text.end)
     assert text.config['cursor'] == 'clock'
 
     for binding, cursor in [('<Leave>', 'clock'),
@@ -47,6 +51,7 @@ def test_add_url_link(monkeypatch, handy_callback):
     monkeypatch.setattr(webbrowser, 'open', stuff.append)
 
     text = teek.Text(teek.Window())
-    links.add_url_link(text, 'asd', URL)
+    text.insert(text.end, 'teek')
+    links.add_url_link(text, URL, text.start, text.end)
     text.get_tag('teek-extras-link-1').bindings['<1>'].run(None)
     assert stuff == [URL]
