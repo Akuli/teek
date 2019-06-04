8d2025e2a7ec356d789017d587e81f53f4a849d6
codetent <22278777+codetent@users.noreply.github.com>
2019-06-04 13:51:03 +0200

    Add methods for finding a font with the right size

--- tests/test_font.py
+++ tests/test_font.py
@@ -3,24 +3,72 @@ import pytest
 import teek
 
 
+def _check_font(font, size):
+    """Check if the size of the given font is equal to the given size.
+       If not, another font with the same size is selected.
+    """
+    orig_family = font.family
+    all_families = teek.Font.families()
+    counter = 0
+
+    while size != font.size:
+        if counter == len(all_families):
+            font.family = orig_family
+            break
+        else:
+            font.family = all_families[counter]
+            counter += 1
+
+    return font
+
+
+def _create_font(font_description):
+    """Create font from given description.
+       If the given font family is not found on the system,
+       a font with the equal size is selected.
+    """
+    font = teek.Font(font_description)
+
+    if isinstance(font_description, str):
+        font_description = font_description.split()
+
+    if len(font_description) >= 2:
+        font = _check_font(font, font_description[1])
+
+    return font
+
+
+def _create_named_font(*args, **kwargs):
+    """Create a named font from given args.
+       If the used font family is not found on the system,
+       a font with the equal size is selected.
+    """
+    font = teek.NamedFont(*args, **kwargs)
+
+    if 'size' in kwargs:
+        font = _check_font(font, kwargs['size'])
+
+    return font
+
+
 def test_font_magic_new_method():
-    font = teek.Font('a_font_with_this_name_does_not_exist')
+    font = _create_font('a_font_with_this_name_does_not_exist')
     assert isinstance(font, teek.Font)
     assert not isinstance(font, teek.NamedFont)
 
     teek.tcl_eval(None, 'font create test_font_name')
-    named_font = teek.Font('test_font_name')
+    named_font = _create_font('test_font_name')
     assert isinstance(named_font, teek.NamedFont)
     named_font.delete()
 
 
 def test_repr_eq_hash():
-    font = teek.Font(('Helvetica', 12))
-    named_font = teek.NamedFont('asda')
+    font = _create_font(('Helvetica', 12))
+    named_font = _create_named_font('asda')
     assert repr(font) == "Font(('Helvetica', 12))"
     assert repr(named_font) == "NamedFont('asda')"
 
-    another_named_font = teek.NamedFont('asda')
+    another_named_font = _create_named_font('asda')
     assert named_font == another_named_font
     assert {named_font: 'toot'}[another_named_font] == 'toot'
 
@@ -38,7 +86,7 @@ def test_repr_eq_hash():
 
 def test_from_and_to_tcl():
     description = ["Helvetica", 42, "bold"]
-    descriptiony_font = teek.Font(description)
+    descriptiony_font = _create_font(description)
     assert descriptiony_font.to_tcl() is description
     assert teek.Font.from_tcl(description) == descriptiony_font
 
@@ -50,8 +98,8 @@ def test_from_and_to_tcl():
 
 
 def test_properties():
-    anonymous_font = teek.Font(("Helvetica", 42, "bold", "underline"))
-    named_font = teek.NamedFont(
+    anonymous_font = _create_font(("Helvetica", 42, "bold", "underline"))
+    named_font = _create_named_font(
         family='Helvetica', size=42, weight='bold', underline=True)
 
     # just to make debugging easier because these facts are needed below
@@ -84,11 +132,11 @@ def test_properties():
 
 
 def test_measure():
-    assert teek.Font(('Helvetica', 42, 'bold')).measure('') == 0
+    assert _create_font(('Helvetica', 42, 'bold')).measure('') == 0
 
 
 def test_metrics():
-    metrics = teek.Font(('Helvetica', 42, 'bold')).metrics()
+    metrics = _create_font(('Helvetica', 42, 'bold')).metrics()
     assert isinstance(metrics['ascent'], int)
     assert isinstance(metrics['descent'], int)
     assert isinstance(metrics['linespace'], int)
@@ -120,7 +168,7 @@ def fonts_are_similar(font1, font2):
 
 
 def test_to_named_font():
-    anonymous = teek.Font(('Helvetica', 42))
+    anonymous = _create_font(('Helvetica', 42))
     named = anonymous.to_named_font()
     assert isinstance(named, teek.NamedFont)
     assert fonts_are_similar(anonymous, named)
@@ -140,5 +188,5 @@ def test_to_named_font():
 
 
 def test_special_font_names():
-    assert isinstance(teek.Font('TkFixedFont'), teek.NamedFont)
+    assert isinstance(_create_font('TkFixedFont'), teek.NamedFont)
     assert isinstance(teek.Font.from_tcl('TkFixedFont'), teek.NamedFont)
