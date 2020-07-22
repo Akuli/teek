c360fbfe086ca09cdd856a8636de05b24e1b7093
Akuli <akuviljanen17@gmail.com>
2020-07-22 17:31:37 +0300

    version bump

--- teek/__init__.py
+++ teek/__init__.py
@@ -4,7 +4,7 @@
 import os as _os
 import sys as _sys
 
-__version__ = '0.5'
+__version__ = '0.6'
 
 if _os.environ.get('READTHEDOCS', None) == 'True':   # pragma: no cover
     # readthedocs must be able to import everything without _tkinter
